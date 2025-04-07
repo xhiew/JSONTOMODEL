b614a86a912ad8076719ef8fa7c4e81481c04bb7
xhiew <hieunx1@vnpay.vn>
2025-04-07 22:20:41 +0700

    init project

---
+++ JSONTOMODEL/JSONTOMODEL/JSONTOMODELApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONTOMODELApp.swift
+//  JSONTOMODEL
+//
+//  Created by xhiew on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct JSONTOMODELApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
